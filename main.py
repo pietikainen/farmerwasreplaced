cd9aadca6332016ee47acbaa5c1108124b1dde9d
Simo Pietikäinen <82802222+pietikainen@users.noreply.github.com>
2024-08-23 18:00:48 +0300

    implement debug mode

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-from navigate import * 
+from movement import * 
 from inventory import *
 from test import *
 from planting import *
 from harvesting import *
 from mazesolver import *
 
-# Define inventory min-max values
-
-
-
-## Reset everything before running
+## Reset playing field before running
 clear()
-# manage_inventory()
 move_to(0, 0)
 
 
+## TODO: Refine general planting function to include "easy" items
 ## Define the item to plant
 item = "Grass"
 
@@ -34,18 +30,20 @@ elif item == "Sunflowers":
     item = Entities.Sunflower
     seed = Items.Sunflower_Seed
 
-# while True:
-#     manage_inventory()
-#     run_maze_solver()
+## Debug mode: 0 = off, 1 = on
+debug = 1
 
-trade(Items.Egg, 50)
-trade(Items.Cactus_Seed, 50)
 
+## Main loop
 while True:
     manage_inventory()
-    # # set_farm_size(6)
-    plant_dinosaurs()
-    harvest_dinosaurs()
+
+    if debug == 1:
+        set_execution_speed(5)
+        set_farm_size(10)
+
+    # plant_dinosaurs()
+    # harvest_dinosaurs()
     # plant_cactus()
     # sort_cactus()
     # harvest_cactus()
@@ -54,6 +52,8 @@ while True:
     # # plant_item(item)
     # # plant_turbo_trees()
     # # plant_turbo_pumpkins()
-    # # polyculture()
+    polyculture()
     # # harvest_item()
+
+    ## Debugging
     # quick_print(get_op_count())
